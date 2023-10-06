dfe1ad9a89a8ab03e40345927c950f03bea3ef43
ITRGaming <raghavbhutra29@gmail.com>
2023-10-06 17:19:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Login from './components/Login'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <NavBar/>
+    // <Login/>
+  );
+}
+
+export default App;
