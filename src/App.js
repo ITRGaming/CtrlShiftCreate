960d588fdd930e841cb23ecaabbf79e79e94e9bd
3Butterfly0 <111911754+3Butterfly0@users.noreply.github.com>
2023-10-07 15:37:36 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import { GlobalStyle } from './GlobalStyle'
 // import Login from './components/Login'
 import NavBar from './components/NavBar'
-import Home from './pages/Home'
+// import Home from './pages/Home'
+import SellerPage from './pages/SellerPage';
+// import CartPage from './pages/CartPage';
 
 function App() {
+
   return (
     <>
-    <NavBar/>
-    <Home/>
-   {/* <Login/> */}
+      <NavBar/>
+      {/* <Home /> */}
+      {/* <Login/> */}
+      <SellerPage />
+{/* <CartPage /> */}
+
+
+
+
+ 
+   {/* <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+   </Router>  */}
     </>
   );
 }
